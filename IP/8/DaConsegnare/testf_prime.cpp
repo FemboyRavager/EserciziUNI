be4e416d3796a5541a1a1826129da1a64e47b730
Emanuele <emanuele.tramonta@gmail.com>
2024-11-15 10:40:06 +0100

    removed redundant case

--- IP/8/DaConsegnare/testf_prime.cpp
+++ IP/8/DaConsegnare/testf_prime.cpp
@@ -5,7 +5,7 @@ bool isPrime(int x) {
   if (x < 0 || x == 0 || x == 1)
     return false;
   for (int i = 2; i < x; i++) {
-    if (x % i == 0 && x != i)
+    if (x % i == 0)
       return false;
   }
   return true;
@@ -22,10 +22,11 @@ int main() {
   for (int i = 0; i < N; i++) {
     cout<<n[i]<<"|";
   }
+  cout<<endl<<endl;
 
   for (int i = 0; i < N; i++) {
     if (isPrime(n[i])) {
-      cout << endl << n[i] << "e' primo\n";
+      cout << endl << n[i] << " e' primo\n";
     }
   }
 }
